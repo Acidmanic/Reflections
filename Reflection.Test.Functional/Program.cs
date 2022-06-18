469226f408848e69a07f76b7e8217fa8d7621193
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-06-18 15:30:30 +0430

    Add Tdd basis

---
+++ Reflection.Test.Functional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection.Test.Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Reflection.Test.Functional/TestBase.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using Acidmanic.Utilities.Reflection;
+
+namespace Reflection.Test.Functional
+{
+    public abstract class TestBase
+    {
+        public abstract void Main();
+
+        protected void PrintObject(object o)
+        {
+            PrintObject("", o);
+        }
+
+        private void PrintObject(string indent, object obj)
+        {
+            if (obj == null)
+            {
+                Console.WriteLine("[NULL]");
+                return;
+            }
+
+            if (TypeCheck.IsCollection(obj.GetType()) && obj is IEnumerable objects)
+            {
+                Console.WriteLine(indent + "[");
+
+                foreach (var o in objects)
+                {
+                    PrintNonEnumerableObject(indent, o);
+                }
+
+                Console.WriteLine(indent + "]");
+            }
+            else
+            {
+                PrintNonEnumerableObject(indent, obj);
+            }
+        }
+
+        private void PrintNonEnumerableObject(string indent, object o)
+        {
+            Line(indent, 30);
+
+            var type = o.GetType();
+
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.CanRead)
+                {
+                    var name = property.Name;
+                    var value = property.GetValue(o);
+
+                    if (value == null || IsSingleLinable(value.GetType()))
+                    {
+                        Console.WriteLine(indent + name + ": " + value);
+                    }
+                    else
+                    {
+                        Console.WriteLine(indent + property.Name);
+
+                        PrintObject(indent + "    ", value);
+                    }
+                }
+            }
+        }
+
+        private void Line(string caption, int lineLength)
+        {
+            var length = lineLength - caption.Length;
+
+            string line = caption;
+
+            for (int i = 0; i < length; i++)
+            {
+                line += "-";
+            }
+
+            Console.WriteLine(line);
+        }
+
+        private bool IsSingleLinable(Type type)
+        {
+            if (type.IsPrimitive || type.IsValueType)
+            {
+                return true;
+            }
+
+            if (type == typeof(string) || type == typeof(char))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
