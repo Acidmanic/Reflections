f627ba62bc39b6f1a90999964a84fdea8a764cb8
Mani Moayedi <acidmanic.moayedi.com>
2022-09-09 22:34:00 +0430

    Add TreatAsLeaf Attribute

---
+++ Acidmanic.Utilities.Reflection/Attributes/TreatAsLeafAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Acidmanic.Utilities.Reflection.Attributes
+{
+    [AttributeUsage(System.AttributeTargets.Property)]
+    public class TreatAsLeafAttribute:Attribute
+    {
+        
+    }
+}
