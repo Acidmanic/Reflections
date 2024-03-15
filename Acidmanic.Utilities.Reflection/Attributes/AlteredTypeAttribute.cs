fe298666498cbd84c8c0d3c49af9c67a91cd3265
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-03-15 22:30:01 +0330

    allow altered types also for structs

--- Acidmanic.Utilities.Reflection/Attributes/AlteredTypeAttribute.cs
+++ Acidmanic.Utilities.Reflection/Attributes/AlteredTypeAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Acidmanic.Utilities.Reflection.Attributes
 {
-    [AttributeUsage(System.AttributeTargets.Class)]
+    [AttributeUsage(System.AttributeTargets.Class | AttributeTargets.Struct)]
     public class AlteredTypeAttribute : Attribute
     {
         public AlteredTypeAttribute(Type alternativeType)
