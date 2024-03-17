2459268feb499c81702b51208a54a7e83b039d99
Mani Moayedi <acidmanic.moayedi@gmail.com>
2024-03-17 15:43:57 +0330

    upgrade to net7.0

---
+++ Reflection.Test.Functional/Tdd019RecordSupport.cs
@@ -0,0 +1,20 @@
+using System;
+using Acidmanic.Utilities.Reflection;
+using Acidmanic.Utilities.Reflection.Attributes;
+using Acidmanic.Utilities.Reflection.Casting;
+using Acidmanic.Utilities.Reflection.ObjectTree;
+using Acidmanic.Utilities.Reflection.ObjectTree.StandardData;
+using Reflection.Test.Functional.TestCaseModels;
+
+namespace Reflection.Test.Functional
+{
+    public class Tdd019RecordSupport : TestBase
+    {
+        
+        
+        public override void Main()
+        {
+           
+        }
+    }
+}
